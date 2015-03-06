99c05518cd92c17495bd66d0908f8779a51e072a
Fulton Wang <fultonw@dhcp-18-111-79-17.dyn.mit.edu>
2015-03-05 22:21:53 -0500

    first

---
+++ recovery_run/recovery_run/constants.py
@@ -0,0 +1,11 @@
+cache_folder = '/Users/fultonw/Documents/projects/runs/recovery_run/cache'
+which_hash = 'sha256'
+lw = 6
+curve_neg_lim = -2
+curve_pos_lim = 50.
+curve_color = 'r'
+shape_color = 'b'
+legend_fontsize = 22
+label_fontsize = 22
+tick_fontsize = 22
+title_fontsize = 22
